f3c6ba99d393c12bcc1ca60e2a5f3fb277be1e74
Joaquin Corchero <joaquin.corchero@gmail.com>
2017-10-05 18:04:59 +0100

    Started to create the controller tests

---
+++ Git.Web/Git.Tests/Controllers/RepositoryControllerTests.cs
@@ -0,0 +1,17 @@
+using Git.Web.Controllers;
+using System;
+using Xunit;
+
+namespace Git.Tests.Controllers
+{
+    public class When_working_with_the_repository_controller
+    {
+        protected RepositoryController _controller;
+
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
