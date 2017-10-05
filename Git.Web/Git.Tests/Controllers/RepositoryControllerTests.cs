fcfb72b384ffaeaff5a9a49d90a4a601ec7a31cc
Joaquin Corchero <joaquin.corchero@gmail.com>
2017-10-05 19:20:29 +0100

    created first test

--- Git.Web/Git.Tests/Controllers/RepositoryControllerTests.cs
+++ Git.Web/Git.Tests/Controllers/RepositoryControllerTests.cs
@@ -1,17 +1,35 @@
 using Git.Web.Controllers;
+using Git.Web.Models;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using Xunit;
+using Xunit.Should;
 
 namespace Git.Tests.Controllers
 {
+    
     public class When_working_with_the_repository_controller
     {
         protected RepositoryController _controller;
 
-        [Fact]
-        public void Test1()
+        public class And_searching_repositories : When_working_with_the_repository_controller
         {
+            ViewResult _viewResult;
 
+            void Execute()
+            {
+                _viewResult = _controller.Index() as ViewResult;
+            }
+
+            [Fact]
+            public void When_no_criteria_is_passed()
+            {
+                var expected = ResponseModel.Empty();
+
+                Execute();
+
+                _viewResult.Model.ShouldBeSameAs(expected);
+            }
         }
     }
 }
---
+++ Git.Web/Git.Web/Models/ResponseModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Git.Web.Models
+{
+    public class ResponseModel
+    {
+        public static ResponseModel Empty()
+        {
+            return new ResponseModel();
+        }
+    }
+}
